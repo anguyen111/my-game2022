e10f3b9908c337dae1f03b48900c6c7db216b87d
anguyen111 <100880135+anguyen111@users.noreply.github.com>
2022-04-08 19:39:56 -0700

    Push

--- _js/gameLogic.js
+++ _js/gameLogic.js
@@ -3,9 +3,11 @@ let canvas = null;
 let ctx = null;
 
 // Variables for game measurements
-let WIDTH = 704;
-let HEIGHT = 288;
-let TILESIZE = 32;
+let TILESIZE = 64;
+let TILESWIDE = 20;
+let TILESHIGH = 9;
+let WIDTH = TILESWIDE*TILESIZE;
+let HEIGHT = TILESHIGH*TILESIZE;
 let BGCOLOR = "blue";
 
 let allSprites = [];
@@ -29,7 +31,7 @@ addEventListener("keydown", function (event) {
     keysDown[event.key] = true;
     console.log(event);
 }, false);
-
+// Event listeners that delete keyboard input
 addEventListener("keyup", function (event) {
     keysUp[event.key] = true;
     delete keysDown[event.key];
@@ -56,12 +58,14 @@ class Sprite {
         this.w = w;
         this.h = h;
         this.color = color;
+        this.speed = 3;
         allSprites.push(this);
     }
     create(x,y,w,h) {
         return new Sprite(x, y, w, h)
     }
-    collideWith(obj) {
+    // snses if objects collide
+    collideWith(obj){
         if (this.x + this.w >= obj.x &&
             this.x <= obj.x + obj.w &&
             this.y + this.h >= obj.y &&
@@ -78,46 +82,39 @@ class Sprite {
     }
 }
 
+// the wall is like a sprite, so it is very similar
 class Wall extends Sprite {
-    constructor(x, y, w, h, color) {
+    constructor (x, y, w, h, color) {
         super(x, y, w, h, color);
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.color = "rgb(0, 200, 200)";
+        this.color = 'rgb(0, 200, 200)';
         allWalls.push(this);
     }
     create(x,y,w,h) {
         return new Wall(x, y, w, h)
     }
-    // update method
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
-    }
 }
 
+// same for cacti
 class Cactus extends Sprite {
-    constructor(x, y, w, h, color) {
+    constructor (x, y, w, h, color) {
         super(x, y, w, h, color);
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
-        this.color = "rgb(0, 255, 0)";
+        this.color = 'rgb(0, 255, 0)';
         allCacti.push(this);
     }
-    create(x,y,w,h, color) {
-        return new Cactus(x, y, w, h, color)
-    }
-    // update method
-    draw() {
-        ctx.fillStyle = this.color;
-        ctx.fillRect(this.x, this.y, this.w, this.h);
+    create(x,y,w,h) {
+        return new Cactus(x, y, w, h)
     }
 }
 
+// the player
 class Player extends Sprite {
     constructor(x, y, w, h, color) {
         super(x, y, w, h, color);
@@ -125,16 +122,18 @@ class Player extends Sprite {
         this.y = y;
         this.vx = 0;
         this.vy = 0;
-        this.speed = 5;
+        this.speed = 2;
         this.gravity = 9.8;
-        this.coFriction = 2;
+        this.coFriction = 1;
         this.w = w;
         this.h = h;
         this.color = color;
         this.health = 100;
-        this.jumpPower = 100;
+        this.jumpPower = TILESIZE;
+        this.canJump = true;
         allSprites.push(this);
     }
+    // how friction is made
     frictionX(){
         if (this.vx > 0.5){
             this.vx -= this.coFriction;
@@ -146,33 +145,34 @@ class Player extends Sprite {
             this.vx = 0;
         }
     }
-    jump () {
+    // how to jump
+    jump() {
         this.vy = -this.jumpPower;
-        console.log(this.timesJumped)
+        console.log(this.timesJumped);
+        console.log(this.vy);
     }
+    // creates the controls
     input() {
-        // if ("w" in keysDown) {
-        //     this.y-=this.speed;
-        // }
-        if (" " in keysDown || "w" in keysDown) {
+        if (" " in keysDown && this.canJump || "w" in keysDown && this.canJump) {
+            // this.vy = -1;
+            this.canJump = false;
             this.jump();
+            console.log("trying to jump...");
         }
         if ("a" in keysDown) {
             this.vx-=this.speed;
         }
-        // if ("s" in keysDown) {
-        //     this.y+=this.speed;
-        // }
         if ("d" in keysDown) {
             this.vx+=this.speed;
         }
     }
     // adding updates....
     update() {
+        console.log(this.x);
         this.vy += this.gravity;
         this.input();
         this.frictionX();
-        this.x += this.vx;
+        this.x += Math.floor(this.vx);
         this.y += this.vy;
         if (this.x > WIDTH-this.w){
             this.x = WIDTH-this.w;
@@ -182,6 +182,7 @@ class Player extends Sprite {
          }
         if (this.y > HEIGHT - this.h){
             this.y = HEIGHT - this.h;
+            this.canJump = true;
          }
         if (this.y < 0){
             this.y = 0;
@@ -193,31 +194,7 @@ class Player extends Sprite {
     }
 }
 
-class Circle {
-    constructor(x, y, w, h, color) {
-        this.x = x;
-        this.y = y;
-        this.w = w;
-        this.h = h;
-        this.color = color;
-        this.speed = 2;
-    }
-    update() {
-        this.x += this.speed*Math.random()*5;
-        this.y += this.speed*Math.random()*5;
-        if (this.x > WIDTH || this.x < 0 || this.y < 0 || this.y > HEIGHT){
-               this.speed*=-1; 
-            }
-    }
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.w, 0, 2 * Math.PI);
-        ctx.stroke();;
-        ctx.fillStyle = this.color;
-        ctx.fill();
-    }
-}
-
+// makes the level design
 function makeGrid(plan, width) {
     let newGrid = [];
     let newRow = [];
@@ -235,11 +212,14 @@ function makeGrid(plan, width) {
     return newGrid;
 }
 
+// reads the level designs
 function readLevel(grid) {
  let startActors = [];
     for (y in grid) {
         for (x in grid[y]) {
+
             let ch = grid[y][x];
+
             if (ch != "\n") {
                 let type = levelChars[ch];
                 if (typeof type == "string") {
@@ -251,18 +231,21 @@ function readLevel(grid) {
                 // console.log(startActors);
             }
         }
-    }
-}
 
+    }
+
+}
+// les computer interpret the grid
 const levelChars = {
     ".": "empty",
     "#": Wall,
-    "|": Cactus,
+    "|": Cactus
 };
 
 console.log(makeGrid(gamePlan, 22));
 readLevel(makeGrid(gamePlan, 22));
 console.log(allWalls);
+
 
 // initialization function
 // creates a div; sets attributes; appends body; creates canvas; puts canvas inside div
@@ -292,39 +275,49 @@ function init() {
 
 
 
-let player = new Player(WIDTH/2, HEIGHT/2, 64, 64, 'rgb(255, 255, 0)');
-console.log(allSprites)
+let player = new Player(WIDTH/2, HEIGHT/2, TILESIZE, TILESIZE, 'rgb(255, 255, 0)');
+console.log(allSprites);
 // let spongeBob = new Sprite(10, 10, 30, 30, 'rgb(255, 255, 0)');
 // let patrick = new Sprite(10, 30, 65, 65, 'rgb(255, 150, 150)');
 // let squidward = new Sprite(70, 90, 20, 20, 'rgb(0, 200, 200)');
 // let sandy = new Circle(70, 200, 25, 40, 'rgb(150, 75, 0)');
 
 
-function input(){
-
-}
-
 function update() {
     player.update();
-    for (i of allCacti){
-        if (i.collideWith(player)){
+    // creates damage
+    for (c of allCacti) {
+        if (c.collideWith(player)) {
             console.log("ouch...");
-            player.health--;
+            if (!player.invulnerable) {
+                player.health-= 10;
+                player.invulnerable = true;
+                setTimeout(() => {player.invulnerable = false}, 2000);
+            }
+        }
+    }
+    // lets player collide with walls
+    for (w of allWalls){
+        if (w.collideWith(player)){
+            console.log("ouch...");
+            player.y = w.y - player.h;
+            player.vy = 0;
+            player.canJump = true;
         }
     }
 
 }
 
+// creates all the sprites
 function draw() {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
     for (i of allSprites){
         i.draw();
     }
-    // drawText(0, 0, 0, 1, "20px Helvetica", "left", "top", "FPS: " + fps, TILESIZE / 2, TILESIZE / 2);
-    drawText(255, 255, 255, 1, "20px Helvetica", "left", "top", "Player Health: " + player.health, TILESIZE / 2, 64);
+    drawText(0, 0, 0, 1, "20px Helvetica", "left", "top", "FPS: " + fps, TILESIZE / 2, TILESIZE / 2);
+    drawText(255, 255, 255, 1, "20px Helvetica", "left", "top", player.health, TILESIZE / 2, 64);
+    
 }
-
-
 
 let fps;
 let then = performance.now();
